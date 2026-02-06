1782bab437e496b8e7d97f4f3a62a4de35173100
Sebatian <dycejr@outlook.com>
2026-02-05 20:04:15 -0800

    Initial Commit

---
+++ hustleXP final1/hustleXP_final1App.swift
@@ -0,0 +1,17 @@
+//
+//  hustleXP_final1App.swift
+//  hustleXP final1
+//
+//  Created by Sebastian Dysart on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct hustleXP_final1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
